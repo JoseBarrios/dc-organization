1ade27a39c5fd9289cc1cd1cd1f43873ef1a3bda
Jose Barrios <jose@cognilab.com>
2018-01-07 15:23:11 -0800

    Publishing for cross-app usage

--- index.js
+++ index.js
@@ -1,179 +1,179 @@
-const Thing = require('@josebarrios/thing');
+const Thing = require('dc-thing');
 const TYPE = 'Organization'
 const EMPTY = '';
 
-class Organization extends Thing {
+class OrganizationDataController extends Thing {
 
-  static get type(){ return TYPE; }
+	static get type(){ return TYPE; }
 
-  constructor(model){
-    model = model || {};
-    super(model);
+	constructor(model){
+		model = model || {};
+		super(model);
 
-    this.address = model.address;
-    this.aggregateRating = model.aggregateRating
-    this.alumni = model.alumni;
-    this.areaServed = model.areaServed;
-    this.award = model.award;
-    this.brand = model.brand;
-    this.contactPoint = model.contactPoint;
-    this.department = model.department;
-    this.dissolutionDate = model.dissolutionDate;
-    this.duns = model.duns;
-    this.email = model.email;
-    this.employee = model.employee;
-    this.event = model.event;
-    this.faxNumber = model.faxNumber;
-    this.founder = model.founder;
-    this.foundingDate = model.foundingDate;
-    this.foundingLocation = model.foundingLocation;
-    this.funder = model.funder;
-    this.globalLocationNumber = model.globalLocationNumber;
-    this.hasOfferCatalog = model.hasOfferCatalog;
-    this.hasPOS = model.hasPOS;
-    this.isicV4 = model.isicV4;
-    this.legalName = model.legalName;
-    this.leiCode = model.leiCode;
-    this.location = model.location;
-    this.logo = model.logo;
-    this.makesOffer = model.makesOffer;
-    this.member = model.member;
-    this.memberOf = model.memberOf;
-    this.naics = model.naics;
-    this.numberOfEmployees = model.numberOfEmployees;
-    this.owns = model.owns;
-    this.parentOrganization = model.parentOrganization;
-    this.review = model.review;
-    this.seeks = model.seeks;
-    this.sponsor = model.sponsor;
-    this.subOrganization = model.subOrganization;
-    this.taxID = model.taxID;
-    this.telephone = model.telephone;
-    this.vatID = model.vatID;
-  }
+		this.address = model.address;
+		this.aggregateRating = model.aggregateRating
+		this.alumni = model.alumni;
+		this.areaServed = model.areaServed;
+		this.award = model.award;
+		this.brand = model.brand;
+		this.contactPoint = model.contactPoint;
+		this.department = model.department;
+		this.dissolutionDate = model.dissolutionDate;
+		this.duns = model.duns;
+		this.email = model.email;
+		this.employee = model.employee;
+		this.event = model.event;
+		this.faxNumber = model.faxNumber;
+		this.founder = model.founder;
+		this.foundingDate = model.foundingDate;
+		this.foundingLocation = model.foundingLocation;
+		this.funder = model.funder;
+		this.globalLocationNumber = model.globalLocationNumber;
+		this.hasOfferCatalog = model.hasOfferCatalog;
+		this.hasPOS = model.hasPOS;
+		this.isicV4 = model.isicV4;
+		this.legalName = model.legalName;
+		this.leiCode = model.leiCode;
+		this.location = model.location;
+		this.logo = model.logo;
+		this.makesOffer = model.makesOffer;
+		this.member = model.member;
+		this.memberOf = model.memberOf;
+		this.naics = model.naics;
+		this.numberOfEmployees = model.numberOfEmployees;
+		this.owns = model.owns;
+		this.parentOrganization = model.parentOrganization;
+		this.review = model.review;
+		this.seeks = model.seeks;
+		this.sponsor = model.sponsor;
+		this.subOrganization = model.subOrganization;
+		this.taxID = model.taxID;
+		this.telephone = model.telephone;
+		this.vatID = model.vatID;
+	}
 
-  get address(){ return this.computed.address; }
-  set address(value){ this.computed.address = value; }
+	get address(){ return this.model.address; }
+	set address(value){ this.model.address = value; }
 
-  get aggregateRating(){ return this.computed.aggregateRating; }
-  set aggregateRating(value){ this.computed.aggregateRating = value; }
-
-
-  get alumni(){ return this.computed.alumni; }
-  set alumni(value){ this.computed.alumni = value; }
-
-  get areaServed(){ return this.computed.areaServed; }
-  set areaServed(value){ this.computed.areaServed = value; }
-
-  get award(){ return this.computed.award; }
-  set award(value){ this.computed.award = value; }
+	get aggregateRating(){ return this.model.aggregateRating; }
+	set aggregateRating(value){ this.model.aggregateRating = value; }
 
 
-  get brand(){ return this.computed.brand; }
-  set brand(value){ this.computed.brand = value; }
+	get alumni(){ return this.model.alumni; }
+	set alumni(value){ this.model.alumni = value; }
 
-  get contactPoint(){ return this.computed.contactPoint; }
-  set contactPoint(value){ this.computed.contactPoint = value; }
+	get areaServed(){ return this.model.areaServed; }
+	set areaServed(value){ this.model.areaServed = value; }
 
-  get department(){ return this.computed.department; }
-  set department(value){ this.computed.department = value; }
+	get award(){ return this.model.award; }
+	set award(value){ this.model.award = value; }
 
-  get dissolutionDate(){ return this.computed.dissolutionDate; }
-  set dissolutionDate(value){ this.computed.dissolutionDate = value; }
 
-  get duns(){ return this.computed.duns; }
-  set duns(value){ this.computed.duns = value; }
+	get brand(){ return this.model.brand; }
+	set brand(value){ this.model.brand = value; }
 
-  get email(){ return this.computed.email; }
-  set email(value){ this.computed.email = value; }
+	get contactPoint(){ return this.model.contactPoint; }
+	set contactPoint(value){ this.model.contactPoint = value; }
 
-  get employee(){ return this.computed.employee; }
-  set employee(value){ this.computed.employee = value; }
+	get department(){ return this.model.department; }
+	set department(value){ this.model.department = value; }
 
-  get event(){ return this.computed.event; }
-  set event(value){ this.computed.event = value; }
+	get dissolutionDate(){ return this.model.dissolutionDate; }
+	set dissolutionDate(value){ this.model.dissolutionDate = value; }
 
-  get faxNumber(){ return this.computed.faxNumber; }
-  set faxNumber(value){ this.computed.faxNumber = value; }
+	get duns(){ return this.model.duns; }
+	set duns(value){ this.model.duns = value; }
 
-  get founder(){ return this.computed.founder; }
-  set founder(value){ this.computed.founder = value; }
+	get email(){ return this.model.email; }
+	set email(value){ this.model.email = value; }
 
-  get foundingDate(){ return this.computed.foundingDate; }
-  set foundingDate(value){ this.computed.foundingDate = value; }
+	get employee(){ return this.model.employee; }
+	set employee(value){ this.model.employee = value; }
 
-  get foundingLocation(){ return this.computed.foundingLocation; }
-  set foundingLocation(value){ this.computed.foundingLocation = value; }
+	get event(){ return this.model.event; }
+	set event(value){ this.model.event = value; }
 
-  get funder(){ return this.computed.funder; }
-  set funder(value){ this.computed.funder = value; }
+	get faxNumber(){ return this.model.faxNumber; }
+	set faxNumber(value){ this.model.faxNumber = value; }
 
-  get globalLocationNumber(){ return this.computed.globalLocationNumber; }
-  set globalLocationNumber(value){ this.computed.globalLocationNumber = value; }
+	get founder(){ return this.model.founder; }
+	set founder(value){ this.model.founder = value; }
 
-  get hasOfferCatalog(){ return this.computed.hasOfferCatalog; }
-  set hasOfferCatalog(value){ this.computed.hasOfferCatalog = value; }
+	get foundingDate(){ return this.model.foundingDate; }
+	set foundingDate(value){ this.model.foundingDate = value; }
 
-  get hasPOS(){ return this.computed.hasPOS; }
-  set hasPOS(value){ this.computed.hasPOS = value; }
+	get foundingLocation(){ return this.model.foundingLocation; }
+	set foundingLocation(value){ this.model.foundingLocation = value; }
 
-  get isicV4(){ return this.computed.isicV4; }
-  set isicV4(value){ this.computed.isicV4 = value; }
+	get funder(){ return this.model.funder; }
+	set funder(value){ this.model.funder = value; }
 
-  get legalName(){ return this.computed.legalName; }
-  set legalName(value){ this.computed.legalName = value; }
+	get globalLocationNumber(){ return this.model.globalLocationNumber; }
+	set globalLocationNumber(value){ this.model.globalLocationNumber = value; }
 
-  get leiCode(){ return this.computed.leiCode; }
-  set leiCode(value){ this.computed.leiCode = value; }
+	get hasOfferCatalog(){ return this.model.hasOfferCatalog; }
+	set hasOfferCatalog(value){ this.model.hasOfferCatalog = value; }
 
-  get location(){ return this.computed.location; }
-  set location(value){ this.computed.location = value; }
+	get hasPOS(){ return this.model.hasPOS; }
+	set hasPOS(value){ this.model.hasPOS = value; }
 
-  get logo(){ return this.computed.logo; }
-  set logo(value){ this.computed.logo = value; }
+	get isicV4(){ return this.model.isicV4; }
+	set isicV4(value){ this.model.isicV4 = value; }
 
-  get makesOffer(){ return this.computed.makesOffer; }
-  set makesOffer(value){ this.computed.makesOffer = value; }
+	get legalName(){ return this.model.legalName; }
+	set legalName(value){ this.model.legalName = value; }
 
-  get member(){ return this.computed.member; }
-  set member(value){ this.computed.member = value; }
+	get leiCode(){ return this.model.leiCode; }
+	set leiCode(value){ this.model.leiCode = value; }
 
-  get memberOf(){ return this.computed.memberOf; }
-  set memberOf(value){ this.computed.memberOf = value; }
+	get location(){ return this.model.location; }
+	set location(value){ this.model.location = value; }
 
-  get naics(){ return this.computed.naics; }
-  set naics(value){ this.computed.naics = value; }
+	get logo(){ return this.model.logo; }
+	set logo(value){ this.model.logo = value; }
 
-  get numberOfEmployees(){ return this.computed.numberOfEmployees; }
-  set numberOfEmployees(value){ this.computed.numberOfEmployees = value; }
+	get makesOffer(){ return this.model.makesOffer; }
+	set makesOffer(value){ this.model.makesOffer = value; }
 
-  get owns(){ return this.computed.owns; }
-  set owns(value){ this.computed.owns = value; }
+	get member(){ return this.model.member; }
+	set member(value){ this.model.member = value; }
 
-  get parentOrganization(){ return this.computed.parentOrganization; }
-  set parentOrganization(value){ this.computed.parentOrganization = value; }
+	get memberOf(){ return this.model.memberOf; }
+	set memberOf(value){ this.model.memberOf = value; }
 
-  get review(){ return this.computed.review; }
-  set review(value){ this.computed.review = value; }
+	get naics(){ return this.model.naics; }
+	set naics(value){ this.model.naics = value; }
 
-  get seeks(){ return this.computed.seeks; }
-  set seeks(value){ this.computed.seeks = value; }
+	get numberOfEmployees(){ return this.model.numberOfEmployees; }
+	set numberOfEmployees(value){ this.model.numberOfEmployees = value; }
 
-  get sponsor(){ return this.computed.sponsor; }
-  set sponsor(value){ this.computed.sponsor = value; }
+	get owns(){ return this.model.owns; }
+	set owns(value){ this.model.owns = value; }
 
-  get subOrganization(){ return this.computed.subOrganization; }
-  set subOrganization(value){ this.computed.subOrganization = value; }
+	get parentOrganization(){ return this.model.parentOrganization; }
+	set parentOrganization(value){ this.model.parentOrganization = value; }
 
-  get taxID(){ return this.computed.taxID; }
-  set taxID(value){ this.computed.taxID = value; }
+	get review(){ return this.model.review; }
+	set review(value){ this.model.review = value; }
 
-  get telephone(){ return this.computed.telephone; }
-  set telephone(value){ this.computed.telephone = value; }
+	get seeks(){ return this.model.seeks; }
+	set seeks(value){ this.model.seeks = value; }
 
-  get vatID(){ return this.computed.vatID; }
-  set vatID(value){ this.computed.vatID = value; }
+	get sponsor(){ return this.model.sponsor; }
+	set sponsor(value){ this.model.sponsor = value; }
+
+	get subOrganization(){ return this.model.subOrganization; }
+	set subOrganization(value){ this.model.subOrganization = value; }
+
+	get taxID(){ return this.model.taxID; }
+	set taxID(value){ this.model.taxID = value; }
+
+	get telephone(){ return this.model.telephone; }
+	set telephone(value){ this.model.telephone = value; }
+
+	get vatID(){ return this.model.vatID; }
+	set vatID(value){ this.model.vatID = value; }
 
 }
 
-module.exports = Organization;
+module.exports = OrganizationDataController;
